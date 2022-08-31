b5e5d98fd4e261c2251858b78ca486363dfacb2d
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:19:21 +0530

    Update 3.Better_Hello_World.java

--- Week 1/3.Better_Hello_World.java
+++ Week 1/3.Better_Hello_World.java
@@ -8,3 +8,8 @@ public class HelloWorld {
      System.out.println("Hello World");
     }
 }
+
+
+/* In order to print things to console we have to write - System.out.println("Hello
+World"). Again for now we should leave System.out.print mean, and should write
+it as it is  */ 
