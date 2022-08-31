4396b4eb24b2799ef71e13e31a3131f95958c4dc
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:06:44 +0530

    Create 11.Integer_and_String.java

---
+++ Week 1/11.Integer_and_String.java
@@ -0,0 +1,10 @@
+package fundamentals;
+import java.util.Scanner;
+public class ArithematicOperators {
+
+    Scanner s = new Scanner(System.in);
+    String str = s.next();
+     int a = s.nextInt();
+    System.out.print(str + " " + a);
+}
+}
