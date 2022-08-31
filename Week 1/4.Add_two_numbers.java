f028287a377464cd8b1af25e43ec2f9611dd96e2
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:21:49 +0530

    Update 4.Add_two_numbers.java

--- Week 1/4.Add_two_numbers.java
+++ Week 1/4.Add_two_numbers.java
@@ -7,3 +7,34 @@ public class AddTwoNumbers {
       System.out.println(c);
     }
 }
+
+
+/* Here, we used variables to store values of two integers and their sum. Thus, a
+variable is a basic unit of storage in a Java program.
+Syntax for Declaring a Variable:   type variable_name [ = value];
+
+
+Here, type is one of Java’s primitive datatypes. The variable_name is the name of
+a variable. We can initialize the variable by specifying an equal sign and a value
+(Initialization is optional). 
+
+However, the compiler never assigns a default value to
+an uninitialized local variable in Java.
+
+
+While writing variable names you should be careful and follow the rules for
+naming them. 
+
+Following are the rules for writing variable names 
+
+
+1. All variable names may contain uppercase and lowercase letters (a-z, A-Z),
+underscore ( _ ), dollar sign ($) and the digits 0 to 9. The dollar sign character
+is not intended for general use. No spaces and no other special characters
+are allowed.
+2. The variable names must not begin with a number.
+3. Java is case-sensitive. Uppercase characters are distinct from lowercase
+characters.
+4. A Java keyword (reserved word) cannot be used as a variable name. 
+
+*/ 
