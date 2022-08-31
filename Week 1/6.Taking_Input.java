d3137111d04a9818c0593bdbec50acc476bed415
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 18:56:18 +0530

    Create 6.Taking_Input.java

---
+++ Week 1/6.Taking_Input.java
@@ -0,0 +1,17 @@
+package fundamentals;
+import java.util.Scanner;
+public class ArithematicOperators {
+    public static void main(String args[]) {
+      int a,b;
+      Scanner s=new Scanner(System.in);
+      a=s.nextInt();
+      b=s.nextInt();
+      int c=a+b;
+      
+      String str=s.nextLine();
+      char ch=str.charAt(0);
+      System.out.println(ch);
+      s.nextDouble();
+      s.nextLong();
+      
+  
