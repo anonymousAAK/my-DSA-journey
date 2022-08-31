7bc880fd4860d9703f630b7e9cb9f5b7fff3811a
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:12:17 +0530

    Create Average_of_two_numbers.java

---
+++ Week 1/Average_of_two_numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Average {
+
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(name);
+        System.out.println((a+b+c)/3);
+		
+	}
+
+}
