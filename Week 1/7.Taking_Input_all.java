92e30f59dde6c43c7f02c979b204cb97b65cdf4d
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:28:19 +0530

    Update 7.Taking_Input_all.java

--- Week 1/7.Taking_Input_all.java
+++ Week 1/7.Taking_Input_all.java
@@ -13,5 +13,23 @@ public class ArithematicOperators {
       System.out.println(ch);
       s.nextDouble();
       s.nextLong();
-      
-  
+    }
+}
+
+/*
+
+The Java Scanner class breaks the input into tokens using a delimiter that is
+whitespace by default. It provides many ways to read and parse various
+primitive values.
+In order to use scanner you have to write this import statement at the top –
+import java.util.Scanner;
+
+
+Sample Input:
+10 5
+
+Here, s.nextInt() scans and returns the next token as int. A token is part of entered
+line that is separated from other tokens by space, tab or newline. So when input
+line is: “10 5” then s.nextInt() returns the first token i.e. “10” as int and s.nextInt()
+again returns the next token i.e. “5” as int  
+  */
