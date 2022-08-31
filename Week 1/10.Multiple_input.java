0b88f9227227e7439a94ec3125bc99e3ce348bce
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:29:47 +0530

    Update 10.Multiple_input.java

--- Week 1/10.Multiple_input.java
+++ Week 1/10.Multiple_input.java
@@ -11,3 +11,24 @@ public class ArithematicOperators {
         System.out.println(str);
     }
 }
+
+/*
+
+
+Other scanner options
+
+
+Some commonly used Scanner class methods are as follows:
+METHOD                                            DESCRIPTION
+public String next()                    It returns the next token from the Scanner.
+public String nextLine()                It moves the Scanner position to the next line and returns the value as a string.
+public byte nextByte()                  It scans the next token as a byte.
+public short nextShort()                It scans the next token as a short value.
+public int nextInt()                    It scans the next token as an int value.
+public long nextLong()                  It scans the next token as a long value.
+public float nextFloat()                It scans the next token as a float value.
+public double nextDouble()              It scans the next token as a double value.
+
+
+
+*/
