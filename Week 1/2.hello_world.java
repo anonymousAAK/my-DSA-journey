3579f85ed9af1bad971b12261f19dace5ec65651
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:18:09 +0530

    Update 2.hello_world.java

--- Week 1/2.hello_world.java
+++ Week 1/2.hello_world.java
@@ -6,3 +6,12 @@ public class HelloWorld {
       System.out.println("Hello World");
     }
 }
+
+/* Consider the following line of code:
+public static void main(String[] args)
+1. This is the line at which the program will begin executing. This statement
+is similar to start block in flowcharts. All Java programs begin execution
+by calling main()
+2. We will understand what public, static, void mean in subsequent
+lectures. For now we should assume that we have to write main as it is.
+3. The curly braces {} indicate start and end of mai                       */
