4273605125fb4681da48c23bf97aa9944bd1cd43
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:01:41 +0530

    Create 9.taking_String_Input.java

---
+++ Week 1/9.taking_String_Input.java
@@ -0,0 +1,10 @@
+package fundamentals;
+import java.util.Scanner;
+public class ArithematicOperators {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String str;
+        str = s.next();
+        System.out.print(str);
+    }
+}
