2d9d099ef45e53a21aaac974b967c9699d9cb50e
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 18:55:35 +0530

    Create 5.Arthemetic_Operation.java

---
+++ Week 1/5.Arthemetic_Operation.java
@@ -0,0 +1,10 @@
+package fundamentals;
+public class ArithematicOperators {
+    public static void main(String args[]) {
+      int a=3;
+      int b=10;
+      int c=b/(2*a);
+      System.out.println(c);
+      
+    }
+}
