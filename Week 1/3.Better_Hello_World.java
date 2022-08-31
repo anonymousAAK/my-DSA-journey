69146ecec41b518dd397ea504a39167cdc6d760e
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:20:19 +0530

    Update 3.Better_Hello_World.java

--- Week 1/3.Better_Hello_World.java
+++ Week 1/3.Better_Hello_World.java
@@ -10,6 +10,9 @@ public class HelloWorld {
 }
 
 
-/* In order to print things to console we have to write - System.out.println("Hello
-World"). Again for now we should leave System.out.print mean, and should write
-it as it is  */ 
+/* In order to print things to console we have to write - 
+System.out.println("HelloWorld").
+Again for now we should leave System.out.print mean, and should write it as it is The built-in method print() is used to display the string which is passed to it. This
+output string is not followed by a newline, i.e., the next output will start on thesame line. 
+
+The built-in method println() is similar to print(), except that println()  outputs a newline after each call. */ 
