563d5a095626f59226286adfc21b87ad49ec0e0a
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 18:49:25 +0530

    Update hello_world.java

--- Week 1/hello_world.java
+++ Week 1/hello_world.java
@@ -1,6 +1,6 @@
 //After creating a class .
 // Here's the first code
-
+package fundamentals;
 public class HelloWorld {
     public static void main(String args[]) {
       System.out.println("Hello World");
