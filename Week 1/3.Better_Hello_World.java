4e5ac1b29d88505b9981ed4577362eadd6b5be8d
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 18:52:33 +0530

    Create 3.Better_Hello_World.java

---
+++ Week 1/3.Better_Hello_World.java
@@ -0,0 +1,10 @@
+package fundamentals;
+public class HelloWorld {
+    public static void main(String args[]) {
+// println prints a new line at the end
+
+     System.out.println("Hello World");
+     System.out.println("Hello World");
+     System.out.println("Hello World");
+    }
+}
