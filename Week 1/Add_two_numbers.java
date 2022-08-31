6520c0b890291c58e7524b8b16e51374ff356987
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 18:54:03 +0530

    Create Add_two_numbers.java

---
+++ Week 1/Add_two_numbers.java
@@ -0,0 +1,9 @@
+package fundamentals;
+public class AddTwoNumbers {
+    public static void main(String args[]) {
+      int a=10;
+      int b=25;
+      int c=a+b;
+      System.out.println(c);
+    }
+}
