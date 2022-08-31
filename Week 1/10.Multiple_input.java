b2289bc9d242338d7794edcea527b41459128be3
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-08-31 19:02:55 +0530

    Create 10.Multiple_input.java

---
+++ Week 1/10.Multiple_input.java
@@ -0,0 +1,13 @@
+package fundamentals;
+import java.util.Scanner;
+public class ArithematicOperators {
+    public static void main(String args[]) {
+
+  
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        String str = s.next();
+        System.out.print(a);
+        System.out.println(str);
+    }
+}
