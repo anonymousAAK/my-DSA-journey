310b8e8f00670568569a2a2d157e63a990296b3b
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-09-03 23:14:00 +0530

    Create 5.Infinite_Loop.java

---
+++ Week 2/5.Infinite_Loop.java
@@ -0,0 +1,12 @@
+public static void main (String[] args) {
+    int x=5;
+    int y=5;
+    while(x==y)
+    {
+        System.out.println("Hello");
+        x++;
+        y++;
+    }
+}
+
+//x and y are equal every time . Hence infinite loop.
