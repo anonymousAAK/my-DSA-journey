3c36d328aca74fc1748890b7f8f8c19c8777efc8
Adarsh Keshri <71527203+anonymousAAK@users.noreply.github.com>
2022-09-12 11:11:27 +0530

    Update and rename 16 to 16.Check_no_Sequence.java

--- Week 3/16.Check_no_Sequence.java
+++ Week 3/16.Check_no_Sequence.java
@@ -94,32 +94,32 @@ print (true)
 
 import java.util.Scanner;
 public class Main {
-public static void main(String[] args) {
-Scanner s = new Scanner(System.in);
-int n = s.nextInt();
-int prev = s.nextInt();
-int count = 2, current;
-boolean isDec = true;
-while(count <= n) {
-current = s.nextInt();
-count++;
-if(current == prev) {
-System.out.println("false");
-return;
-}
-if(current < prev) {
-if(isDec == false) {
-System.out.println("false");
-return;
-}
-}
-else {
-if(isDec == true) {
-isDec = false;
-}
-}
-prev = current;
-}
-System.out.println("true");
+     public static void main(String[] args) {
+          Scanner s = new Scanner(System.in);
+          int n = s.nextInt();
+          int prev = s.nextInt();
+          int count = 2, current;
+          boolean isDec = true;
+          while(count <= n) {
+              current = s.nextInt();
+              count++;
+              if(current == prev) {
+                  System.out.println("false");
+                    return;
+                  }
+              if(current < prev) {
+                    if(isDec == false) {
+                        System.out.println("false");
+                        return;
+             }
+              }
+                     else {
+                        if(isDec == true) {
+                            isDec = false;
+                }
+           }
+                 prev = current;
+       }
+          System.out.println("true");
 }
 }
